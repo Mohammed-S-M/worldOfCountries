f6b3e02a2ea90dabfa0c3c7a1ea86d4fa34db450
Mohammed S Mahmood <mohammedsmahmood88@live.com>
2022-07-14 11:02:23 -0400

    add babel and strict mode

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
@@ -6,4 +6,8 @@ import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
